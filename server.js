41b6cd1e93eba5872a5f735d5ac9f8d5ea1d3936
stephen holland <stephenholland279@gmail.com>
2021-08-13 08:22:22 +0100

    seeion issues

--- server.js
+++ server.js
@@ -114,7 +114,7 @@ const redirectlogin = (req, res) => {
 
 }
 
-app.get('/home', (req, res, next) => {
+app.get('/', (req, res, next) => {
 	console.log("inside and userId "+ req.session.user);
 
 	const { user } = req.session.user;
