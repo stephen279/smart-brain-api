75ac2a98b207a00c5436896b40a0af2deb7ebe5b
stephen holland <stephenholland279@gmail.com>
2021-08-15 11:45:14 +0100

    seeion issues

--- server.js
+++ server.js
@@ -165,7 +165,7 @@ app.post('/signin', (req, res) => {
 						console.log("session set --------"+req.session.user);
 						console.log(user);
 						//res.status(200).json('session set with '+req.session.user)
-						res.json(user[0],req.session.user)
+						res.json(user[0])
 					})
 					.catch(err => res.status(400).json('unable to get user and no seesion set '+req.session.user))
 				
