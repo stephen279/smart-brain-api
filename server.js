dda5e1bfb608fd2f0bef1b9a4a89264bb8216bf5
stephen holland <stephenholland279@gmail.com>
2021-08-20 10:53:16 +0100

    seeion issues

--- server.js
+++ server.js
@@ -164,6 +164,7 @@ app.post('/signin', (req, res) => {
 						req.session.authenticated = true;
 						req.session.user = req.body.email;
 						console.log(req.session);
+						 res.redirect("/home");
       			//	res.redirect("/shop");
 					
 					//	res.status(200).json('session set with '+req.sessionID)
