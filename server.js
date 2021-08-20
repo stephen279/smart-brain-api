bc4575be2f33759d63ee54be9d9363137c8dd33a
stephen holland <stephenholland279@gmail.com>
2021-08-20 21:49:18 +0100

    seeion issues

--- server.js
+++ server.js
@@ -98,12 +98,7 @@ app.use(session({
 }))
 
 
-app.use((req, res,next) => {
-		console.log(req.session);
-	//console.log("inside beginning req seeion -----------"+req.session);
 
-	next();
-})
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
