13c45396ac2a8a4c1dab07fdd2890025df363974
stephen holland <stephenholland279@gmail.com>
2021-08-20 16:57:42 +0100

    seeion issues

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const knex = require('knex');
 const { response } = require('express');
 var session = require('express-session');
-let sess = req.session;
+
 
 //const baseURL = "http://localhost:3001/"
 
